bc2c1693547e51942390b0ca80469cbc1f550efb
Pedro Augusto Soares Pereira <CIANDT\pedrosoares@lnb028338cps.ciandt.global>
2021-03-22 11:00:55 -0300

    Adicionando Post Router

--- src/main/gateways/http/router.js
+++ src/main/gateways/http/router.js
@@ -2,5 +2,10 @@ module.exports = (router) =>{
     router.get('/toggles', (req, res) => {
         res.send(['Rede', 'number']);
     });
+
+    router.post('/toggle', (req, res) => {
+        res.status(200).send('Deu Sucesso!');
+    });
+
     return router;
-} 
+}
