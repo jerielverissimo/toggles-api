57c4015b7c55c8cbbeb0e7feec39625341e941d0
Bruno Henrique Martins da Fonseca <CIANDT\bfonseca@lnb028607cps.ciandt.global>
2021-03-22 11:53:49 -0300

    Implementando tratamento de erro no toggle controller

--- src/main/gateways/http/toggleController.js
+++ src/main/gateways/http/toggleController.js
@@ -1,6 +1,12 @@
 const toggleController= {
     async add(req, res) {
-        res.status(200).send('Deu Sucesso!');
+        try{
+            res.status(200).send('Deu Sucesso!');            
+        }
+        catch(error){
+            console.log(error);
+            res.status(500).send(`${error}`);
+        }
     }
 };
 
