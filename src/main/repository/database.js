c39b9bd4827529a4349ee7445ca9586d8926be4a
Lucas Henriques Pereira <CIANDT\lucaspereira@lnb028378cps.ciandt.global>
2021-03-22 14:14:53 -0300

    Ajustando database

--- src/main/repository/database.js
+++ src/main/repository/database.js
@@ -1,3 +1,19 @@
-const database={};
+const { MongoClient } = require("mongodb");
 
+const uri = "mongodb://localhost:27017"
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}; 
+
+const database={
+    async upsert(document){
+        try {
+        const conn = new MongoClient (uri, options)
+        } catch (error) {
+        
+        }    
+    }
+};
 module.exports=database;
